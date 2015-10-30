88b4705c3a02e97703e4e5317fa02b7f1de1a53d
Christian Petrov <cpetrov@eclipsesource.com>
2015-10-30 15:43:25 +0100

    Bundle connect distribution dependencies
    Done to make the editor easier to consume on connect.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    clean: ["dist"],
+    concat: {
+      js: {
+        src: [
+          "node_modules/codemirror/lib/codemirror.js",
+          "node_modules/codemirror/addon/hint/show-hint.js",
+          "node_modules/codemirror/addon/edit/closetag.js",
+          "node_modules/codemirror/addon/edit/closebrackets.js",
+          "node_modules/codemirror/addon/edit/matchbrackets.js",
+          "node_modules/codemirror/addon/selection/active-line.js",
+          "node_modules/codemirror/addon/runmode/runmode.js",
+          "node_modules/codemirror/addon/tern/tern.js",
+          "node_modules/codemirror/mode/javascript/javascript.js",
+          "node_modules/acorn/dist/acorn.js",
+          "node_modules/acorn/dist/acorn_loose.js",
+          "node_modules/acorn/dist/walk.js",
+          "node_modules/tern/lib/signal.js",
+          "node_modules/tern/lib/tern.js",
+          "node_modules/tern/lib/def.js",
+          "node_modules/tern/lib/comment.js",
+          "node_modules/tern/lib/infer.js",
+          "node_modules/codemirror-extension/addon/hint/templates-hint.js",
+          "node_modules/codemirror-extension/addon/hover/text-hover.js",
+          "node_modules/codemirror-javascript/addon/hint/tern/tern-extension.js",
+          "node_modules/codemirror-javascript/demo/defs/ecma5.json.js",
+          "node_modules/codemirror-javascript/addon/hint/javascript/javascript-templates.js",
+          "node_modules/codemirror-javascript/addon/hint/tern/tern-hover.js",
+          "codemirror/addon/hint/javascript/tabris-templates.js",
+          "codemirror/addon/hint/context-autocomplete-hint.js",
+          "tabris.js"
+        ],
+        dest: "dist/<%= pkg.name %>-connect.js"
+      },
+      css: {
+        src: [
+          "node_modules/codemirror/lib/codemirror.css",
+          "node_modules/codemirror/theme/eclipse.css",
+          "node_modules/codemirror-javascript/addon/hint/tern/tern-extension.css",
+          "node_modules/codemirror-extension/addon/hint/show-hint-eclipse.css",
+          "node_modules/codemirror-extension/addon/hint/show-context-info.css",
+          "node_modules/codemirror-extension/addon/hint/templates-hint.css",
+          "node_modules/codemirror-extension/addon/hover/text-hover.css"
+        ],
+        dest: "dist/<%= pkg.name %>-connect.css"
+      }
+    },
+    uglify: {
+      js: {
+        src: "<%= concat.js.dest %>",
+        dest: "dist/<%= pkg.name %>-connect.min.js"
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.registerTask("build", ["clean", "concat", "uglify"]);
+};
